b363a72e37d0c900f918b5c7eefafe6babb25d6f
luabarbosa <lua.barbosa@yahoo.com.br>
2022-09-01 11:32:24 -0300

    fix: eliminando erros

--- src/main/resources/scripts.sql
+++ src/main/resources/scripts.sql
@@ -1,3 +1,5 @@
+create sequence hibernate_sequence start with 1 increment by 1;
+
 create table users (id bigint not null, city varchar(255), age integer not null, name varchar(255), primary key (id));
 
 insert into users (id, city, age, name) values (1, 'Salvador', 24,'Maria Luiza Mota');
